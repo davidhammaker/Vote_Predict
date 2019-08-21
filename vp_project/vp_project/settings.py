28dc1479b37115f3c56e36f8e0dc47aaa613f2e8
David J. Hammaker <davidjosh93@gmail.com>
2019-08-20 22:27:21 -0400

    Update 'INSTALLED_APPS'
    Include 'rest_framework' and 'vp_app' in installed apps within 
    'settings.py'.

--- vp_project/vp_project/settings.py
+++ vp_project/vp_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'vp_app.apps.VpAppConfig',
 ]
 
 MIDDLEWARE = [
