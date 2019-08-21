0910a67f3bbb7ce720b167a15874e92721dd6ffd
David J. Hammaker <davidjosh93@gmail.com>
2019-08-20 22:28:51 -0400

    Swap out generated 'SECRET_KEY' for environment variable

--- vp_project/vp_project/settings.py
+++ vp_project/vp_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@p6uj&rk^m^3s(8@#m$!$e@o$no_-ku7fuylw*sa1193k_0(w^'
+SECRET_KEY = os.environ.get("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
