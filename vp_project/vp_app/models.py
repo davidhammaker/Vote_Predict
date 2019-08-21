e2b05fa77604401ddcdd11b42f5485002e350595
David J. Hammaker <davidjosh93@gmail.com>
2019-08-20 22:54:43 -0400

    Create 'Question' and 'Answer' models
    Create models for the foundation of the 'Vote Predict' app.

--- vp_project/vp_app/models.py
+++ vp_project/vp_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.CharField(max_length=128)
+    date_revealed = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
+
+
+class Answer(models.Model):
+    content = models.CharField(max_length=64)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
