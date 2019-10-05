d417a1972426b6829f4083c43baba79b89493d19
David J. Hammaker <davidjosh93@gmail.com>
2019-10-05 17:37:05 -0400

    Update database models
    Update 'Question' and 'Answer'. Add 'Response' and 'Record'. Remove 
    'db.sqlite3' from repo. Add database items to admin site.

--- vp_project/vp_app/admin.py
+++ vp_project/vp_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+
+class AnswerInLine(admin.StackedInline):
+    model = Answer
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'content',
+        'date_published'
+    ]
+    inlines = [AnswerInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
--- vp_project/vp_app/models.py
+++ vp_project/vp_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
     content = models.CharField(max_length=128)
-    date_revealed = models.DateTimeField(default=timezone.now)
+    date_published = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.content
@@ -12,7 +13,50 @@ class Question(models.Model):
 
 class Answer(models.Model):
     content = models.CharField(max_length=64)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question,
+        related_name='answers',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.content
+
+
+class Response(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='responses',
+        on_delete=models.CASCADE
+    )
+    question = models.ForeignKey(
+        Question,
+        related_name='responses',
+        on_delete=models.CASCADE
+    )
+    vote = models.ForeignKey(
+        Answer,
+        related_name='votes',
+        on_delete=models.CASCADE
+    )
+    prediction = models.ForeignKey(
+        Answer,
+        related_name='predictions',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.question
+
+
+class Record(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='record',
+        on_delete=models.CASCADE
+    )
+    total_responses = models.IntegerField()
+    correct_predictions = models.IntegerField()
+
+    def __str__(self):
+        return self.user
