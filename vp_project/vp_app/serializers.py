518f925a6c130afcb040af9fc74cf1bae6ed1bc0
David J. Hammaker <davidjosh93@gmail.com>
2019-10-19 15:04:35 -0400

    Update 'ResponseSerializer'
    Expand validation to ensure that 'Response' instances represent valid 
    votes and predictions.

--- vp_project/vp_app/serializers.py
+++ vp_project/vp_app/serializers.py
@@ -27,20 +27,46 @@ class AnswerSerializer(serializers.ModelSerializer):
 
 class ResponseSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.id')
+    question = serializers.ReadOnlyField(source='question.id')
 
     def validate(self, data):
         """
         Check that a user has not yet submitted a response for the given
-        question. Users may only respond once per question.
+        question. Users may only respond once per question. Check that
+        the provided vote and prediction pertain to the question.
         """
+        question_id = self.context['request'] \
+            .parser_context['kwargs']['question_id']
+        answers = [
+            answer for answer in
+            Question.objects.get(id=question_id).answers.all()
+        ]
+        answer_ids = [answer.id for answer in answers]
+
+        # Verify that the user has not previously responded to this
+        # question.
         existing_response = Response.objects.filter(
-            user=self.context["request"].user.id,
-            question=data['question']
+            user=self.context['request'].user.id,
+            question=question_id
         ).first()
         if existing_response:
             raise serializers.ValidationError(
                 'Users may submit only one response per question.'
             )
+
+        # Verify that the response contains a valid vote.
+        if data['vote'].id not in answer_ids:
+            raise serializers.ValidationError(
+                f'Invalid vote. Choose one of the following: {answers}.'
+            )
+
+        # Verify that the response contains a valid prediction.
+        if data['prediction'].id not in answer_ids:
+            raise serializers.ValidationError(
+                f'Invalid prediction. Choose one of the following: '
+                f'{answers}.'
+            )
+
         return data
 
     class Meta:
