bcf847335509f1ea953a51e8cb088c933b9860d2
David J. Hammaker <davidjosh93@gmail.com>
2019-11-02 01:59:29 -0400

    Prevent CORS-related errors
    Install package and update settings to prevent CORS-related errors 
    associated with frontend-to-backend HTTP requests.

--- vp_project/vp_project/settings.py
+++ vp_project/vp_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'vp_app.apps.VpAppConfig',
     'users.apps.UsersConfig',
 ]
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'vp_project.urls'
@@ -122,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CORS Configuration
+CORS_ORIGIN_ALLOW_ALL = True
